7359ecaeb62106db21a18d34080672c8bafa1f2b
JadenSimon <sijaden@amazon.com>
2022-03-18 17:41:22 -0700

    64bit

--- scripts/test/test.ts
+++ scripts/test/test.ts
@@ -21,7 +21,7 @@ import { setupVSCodeTestInstance } from './launchTestUtilities'
             extensionDevelopmentPath: rootDir,
             extensionTestsPath: testEntrypoint,
             // For verbose VSCode logs, add "--verbose --log debug". c2165cf48e62c
-            launchArgs: [testWorkspace, '--verbose', '--log', 'debug'],
+            launchArgs: [testWorkspace],
             extensionTestsEnv: {
                 ['DEVELOPMENT_PATH']: rootDir,
                 ['AWS_TOOLKIT_AUTOMATION']: 'unit',
